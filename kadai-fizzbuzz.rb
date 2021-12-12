3488231db824e1fc0cf7abdfa59df96f9e56eb01
kouhei.nogawa <nk04084163@gmail.com>
2021-12-12 07:19:59 +0000

    kadai-fizzbuzz.rb

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,20 +1,18 @@
 def fizzbuzz(num)
  
     if (num % 3 == 0) && (num % 5 == 0)
-        puts 'FizzBuzz'
-        
+       return "FizzBuzz"
     elsif num % 5 == 0
-        puts 'Buzz'
-        
+       return "Buzz"
     elsif num % 3 == 0
-        puts 'Fizz'
-        
+       return "Fizz"
     else
-        puts num
+       return num
     end 
 end
-
-num_max = 100
-(1..num_max).each do |num|
-  fizzbuzz(num)
-end
+    
+    num_max = 100
+    (1..num_max).each do |num|
+        puts fizzbuzz(num)
+    end
+    
